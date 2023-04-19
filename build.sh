fa5d86121efc4334e3cf31ec6fc364b7a8948397
Thomas Steiner <tomac@google.com>
2023-04-19 15:31:22 +0200

    Space

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-emcc -g althttpd.c -o althttpd.js -s FORCE_FILESYSTEM=1 -s INVOKE_RUN=0 -lwebsocket.js -s PROXY_POSIX_SOCKETS -s USE_PTHREADS -s PROXY_TO_PTHREAD -s EXPORTED_RUNTIME_METHODS="['callMain', 'FS']"
+emcc -g althttpd.c -o althttpd.js -s FORCE_FILESYSTEM=1 -s INVOKE_RUN=0 -l websocket.js -s PROXY_POSIX_SOCKETS -s USE_PTHREADS -s PROXY_TO_PTHREAD -s EXPORTED_RUNTIME_METHODS="['callMain', 'FS']"
